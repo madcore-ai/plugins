ba9a924aa484ba32e8717aa7194e9036c0059586
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-21 11:42:37 +0200

    Add slack push/pull jobs

---
+++ pods/slack/jobs/madcore_plugins_slack_pull.groovy
@@ -0,0 +1,21 @@
+job('madcore.plugin.slack.pull') {
+    wrappers { preBuildCleanup() }
+    parameters {
+        stringParam('APP_NAME', 'slack', '')
+        stringParam('CHANNELS', 'tests', 'Comma separated list of slack channels')
+        stringParam('S3_BUCKET', '', 'S3 bucket name')
+        booleanParam('ALL_CHANNELS', false, 'Check if you want to ignore channels and get all of them')
+    }
+    steps {
+        def command = """#!/bin/bash
+echo "CHANNELS: '\$CHANNELS'"
+echo "S3_BUCKET: '\$S3_BUCKET'"
+echo "ALL_CHANNELS: '\$ALL_CHANNELS'"
+
+    pushd /opt/madcore/bin
+        python run_cmd_in_container.py "\$APP_NAME" "madcore_slack -a pull_messages -p '{\\"channels\\": \\"\$CHANNELS\\", \\"s3_bucket\\":\\"\$S3_BUCKET\\", \\"all_channels\\":\$ALL_CHANNELS}'"
+    popd
+"""
+        shell(command)
+    }
+}
---
+++ pods/slack/jobs/madcore_plugins_slack_push.groovy
@@ -0,0 +1,19 @@
+job('madcore.plugin.slack.push') {
+    wrappers { preBuildCleanup() }
+    parameters {
+        stringParam('APP_NAME', 'slack', '')
+        stringParam('CHANNEL', 'tests', 'Channel name to post message to.')
+        stringParam('MESSAGE', '', 'Message text to post.')
+    }
+    steps {
+        def command = """#!/bin/bash
+echo "CHANNEL: '\$CHANNEL'"
+echo "MESSAGE: '\$MESSAGE'"
+
+    pushd /opt/madcore/bin
+        python run_cmd_in_container.py "\$APP_NAME" "madcore_slack -a post_message -p '{\\"channel\\": \\"\$CHANNEL\\", \\"text\\":\\"\$MESSAGE\\"}'"
+    popd
+"""
+        shell(command)
+    }
+}

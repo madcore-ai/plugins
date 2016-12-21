b113816e4451965c54618e8b4258641bea332c19
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-21 12:35:30 +0200

    Modify slack deploy job

--- pods/slack/jobs/madcore_plugin_slack_deploy.groovy
+++ pods/slack/jobs/madcore_plugin_slack_deploy.groovy
@@ -2,7 +2,7 @@ pipelineJob('madcore.plugin.slack.deploy') {
     parameters {
         stringParam('REPO_URL', 'https://github.com/madcore-ai/containers', '')
 	    stringParam('APP_NAME', 'slack', '')
-	    stringParam('SECRET', 'slack-token=NOTOKENHERE;slack-test=test', '')
+	    stringParam('SLACK_TOKEN', '', 'Specify the API slack token.')
     }
 
     definition {
@@ -11,7 +11,7 @@ pipelineJob('madcore.plugin.slack.deploy') {
             script("""
             node {
                 stage 'Docker: build container'
-                build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME)]
+                build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'DOCKERFILE_PATH', value: params.APP_NAME)]
                 stage 'Docker: registry publish'
                 build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
                 stage 'Docker: registry status'
@@ -19,7 +19,7 @@ pipelineJob('madcore.plugin.slack.deploy') {
                 stage 'Kubernetes: create'
                 build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'slack/kub')]
                 stage 'Kubernetes: create secret'
-                build job: 'madcore.kubectl.create.secret', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SECRET', value: params.SECRET)]
+                build job: 'madcore.kubectl.create.secret', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SECRET', value: 'slack-token=' + params.SLACK_TOKEN)]
                 }
             """.stripIndent())
 	    }

4e08125e468dd79e36a67e53dc14a0b695e1f1ac
Chirica Gheorghe <chiricagheorghe@gmail.com>
2016-12-21 07:19:11 +0200

    Add slack plugin - basics

---
+++ pods/slack/jobs/madcore_plugin_slack_deploy.groovy
@@ -0,0 +1,27 @@
+pipelineJob('madcore.plugin.slack.deploy') {
+    parameters {
+        stringParam('REPO_URL', 'https://github.com/madcore-ai/containers', '')
+	    stringParam('APP_NAME', 'slack', '')
+	    stringParam('SECRET', 'slack-token=xoxp-102271888193-108964168741-116823616578-24874f6421c8051ac92247b017277533;slack-test=test', '')
+    }
+
+    definition {
+	    cps {
+            sandbox()
+            script("""
+            node {
+                stage 'Docker: build container'
+                build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME)]
+                stage 'Docker: registry publish'
+                build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                stage 'Docker: registry status'
+                build job: 'madcore.docker.registry.status', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                stage 'Kubernetes: create'
+                build 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', 'slack/kub')]
+                stage 'Kubernetes: create secret'
+                build 'madcore.kubectl.create.secret', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SECRET', value: params.SECRET)]
+                }
+            """.stripIndent())
+	    }
+    }
+}

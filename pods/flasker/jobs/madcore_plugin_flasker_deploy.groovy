ec67e42fb2586801567abc015cd813c6bf0defea
Denis Sokolov <hantden@gmail.com>
2017-01-14 17:44:32 +0200

    fix syntax

--- pods/flasker/jobs/madcore_plugin_flasker_deploy.groovy
+++ pods/flasker/jobs/madcore_plugin_flasker_deploy.groovy
@@ -12,22 +12,22 @@ pipelineJob('madcore.plugin.flasker.deploy') {
             sandbox()
             script("""
             node {
-                stage 'Docker: build container' {
+                stage ('Docker: build container') {
                   build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'DOCKERFILE_PATH', value: params.DOCKERFILE_PATH)]
                 }
-                stage 'Docker: registry publish' {
+                stage ('Docker: registry publish') {
                   build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
                 }
-                stage 'Docker: registry status' {
+                stage ('Docker: registry status') {
                   build job: 'madcore.docker.registry.status', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
                 }
-                stage 'Kubernetes: create' {
+                stage ('Kubernetes: create') {
                   build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'flasker/kub')]
                 }
-                stage 'Update app base' {
+                stage ('Update app base') {
                   build job: 'madcore.redis.app.update', parameter: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'APP_PORT', value: params.PORT)]
                 }
-                stage 'Update certificate and haproxy' {
+                stage ('Update certificate and haproxy') {
                   build job: 'madcore.ssl.letsencrypt.getandinstall'
                 }
               }

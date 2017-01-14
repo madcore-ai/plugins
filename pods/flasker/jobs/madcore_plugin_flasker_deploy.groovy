5a846d87fc2985eed009ff771eb874e60065c6dc
Denis Sokolov <hantden@gmail.com>
2017-01-14 17:39:28 +0200

    fix syntax

--- pods/flasker/jobs/madcore_plugin_flasker_deploy.groovy
+++ pods/flasker/jobs/madcore_plugin_flasker_deploy.groovy
@@ -12,19 +12,25 @@ pipelineJob('madcore.plugin.flasker.deploy') {
             sandbox()
             script("""
             node {
-                stage 'Docker: build container'
-                build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'DOCKERFILE_PATH', value: params.DOCKERFILE_PATH)]
-                stage 'Docker: registry publish'
-                build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
-                stage 'Docker: registry status'
-                build job: 'madcore.docker.registry.status', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
-                stage 'Kubernetes: create'
-                build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'flasker/kub')]
-                stage 'Update app base'
-                build job: 'madcore.redis.app.update', parameter: [string(name: 'APP_NAME', value: params.APP_NAME)]
-                stage 'Update certificate and haproxy'
-                build job: 'madcore.ssl.letsencrypt.getandinstall'
+                stage 'Docker: build container' {
+                  build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'DOCKERFILE_PATH', value: params.DOCKERFILE_PATH)]
                 }
+                stage 'Docker: registry publish' {
+                  build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                }
+                stage 'Docker: registry status' {
+                  build job: 'madcore.docker.registry.status', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                }
+                stage 'Kubernetes: create' {
+                  build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'flasker/kub')]
+                }
+                stage 'Update app base' {
+                  build job: 'madcore.redis.app.update', parameter: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'APP_PORT', value: params.PORT)]
+                }
+                stage 'Update certificate and haproxy' {
+                  build job: 'madcore.ssl.letsencrypt.getandinstall'
+                }
+              }
             """.stripIndent())
 	    }
     }

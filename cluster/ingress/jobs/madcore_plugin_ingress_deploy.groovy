1a8cd9ec10a39382c823943aea0e3d309a7bde4f
Denys Sokolov <hantden@gmail.com>
2017-02-09 15:56:53 +0200

    add label, change deploy ingress job

--- cluster/ingress/jobs/madcore_plugin_ingress_deploy.groovy
+++ cluster/ingress/jobs/madcore_plugin_ingress_deploy.groovy
@@ -6,24 +6,11 @@ pipelineJob('madcore.plugin.ingress.deploy') {
 	    cps {
             sandbox()
             //script("""
-            @NonCPS
-            def shellCommand (String command) {
-              return output = ['bash', '-c', command].execute().in.text
-            }
             node {
                 stage('Ingress: cloud formation install') {
                 }
-                stage ('Initialize ingress node') {
-                  node_name = shellCommand ("ssh -i /home/ubuntu/id_rsa -i /home/ubuntu/.ssl/id_rsa -o StrictHostKeychecking=no ubuntu@${IngressInstanceIP} \"echo \\\$HOSTNAME\"")
-                  def status
-                  until ( status == "Ready") {
-                    sleep 5
-                    status = shellCommand ("kubectl get nodes | grep ${node_name} | awk '{print \$2}'")
-                  }
-                  sh "kubectl label node ${node_name} role=loadbalancer"
-                }
                 stage('Ingress: deploy pods') {
-                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'pods/ingress/kub')]
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'cluster/ingress/kub')]
                 }
               }
             //""".stripIndent())

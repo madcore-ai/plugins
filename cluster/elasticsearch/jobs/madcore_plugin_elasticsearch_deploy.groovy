cc8b7d1e17e7f4d3a0eb0615d68ce8b6645f49d0
Denys Sokolov <hantden@gmail.com>
2017-03-24 20:25:56 +0200

    reconfigure es cluster

--- cluster/elasticsearch/jobs/madcore_plugin_elasticsearch_deploy.groovy
+++ cluster/elasticsearch/jobs/madcore_plugin_elasticsearch_deploy.groovy
@@ -8,11 +8,28 @@ pipelineJob('madcore.plugin.elasticsearch.deploy') {
             sandbox()
             script("""
             node {
-                stage('Elasticsearch: install') {
-                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'cluster/elasticsearch/kub')]
+                stage('Ingress: cloud formation install') {
+                }
+                stage('Elasticsearch: create services') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'cluster/elasticsearch/kub/services')]
+                }
+                stage('Elasticsearch: create master') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'cluster/elasticsearch/kub/es-master-rc.yaml')]
+                }
+                stage('Elasticsearch: wait for master') {
+                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'es-master')]
+                }
+                stage('Elasticsearch: create client') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'cluster/elasticsearch/kub/es-client-rc.yaml')]
+                }
+                stage('Elasticsearch: wait for client') {
+                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'es-client')]
+                }
+                stage('Elasticsearch: create data') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'cluster/elasticsearch/kub/es-data-rc.yaml')]
                 }
                 stage('Elasticsearch: wait for elasticsearch cluster to start') {
-                    build job: 'madcore.kubectl.wait.service.up', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: 'es-master:9200'), string(name: 'SERVICE_NAMESPACE', value: 'es-cluster')]
+                    build job: 'madcore.kubectl.wait.service.up', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: 'elasticsearch:9200'), string(name: 'SERVICE_NAMESPACE', value: 'es-cluster')]
                 }
                 }
             """.stripIndent())

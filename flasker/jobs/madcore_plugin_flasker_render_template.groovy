6fae7cb498ec45e0ca7c1509b4c62f934d58c693
Denys Sokolov <hantden@gmail.com>
2017-04-06 11:10:54 +0300

    fix syntax

--- flasker/jobs/madcore_plugin_flasker_render_template.groovy
+++ flasker/jobs/madcore_plugin_flasker_render_template.groovy
@@ -5,10 +5,10 @@ piplineJob('madcore.plugin.flasker.render_template') {
 
     stages {
       stage ('rendering rc template') {
-        cat /opt/madcore/plugins/flasker/templates/rc.yaml.template | sed -e "s/\${APP_NAME}/\$APP_NAME/g" > /opt/madcore/plugins/flasker/kub/rc.yaml
+        sh(cat /opt/madcore/plugins/flasker/templates/rc.yaml.template | sed -e "s/\${APP_NAME}/\$APP_NAME/g" > /opt/madcore/plugins/flasker/kub/rc.yaml)
       }
       stage ('rendering svc template') {
-        cat /opt/madcore/plugins/flasker/templates/svc.yaml.template | sed -e "s/\${APP_NAME}/\$APP_NAME/g" > /opt/madcore/plugins/flasker/kub/svc.yaml
+        sh(cat /opt/madcore/plugins/flasker/templates/svc.yaml.template | sed -e "s/\${APP_NAME}/\$APP_NAME/g" > /opt/madcore/plugins/flasker/kub/svc.yaml)
       }
    }
 }

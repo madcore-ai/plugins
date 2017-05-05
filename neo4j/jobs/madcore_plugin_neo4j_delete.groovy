42f91c0aa44057673bb4097164fc565df343af42
Nand Singh <nand@madcore.ai>
2017-05-05 17:16:03 +0100

    Initial changes for neo4j

---
+++ neo4j/jobs/madcore_plugin_neo4j_delete.groovy
@@ -0,0 +1,24 @@
+pipelineJob('madcore.plugin.neo4j.delete') {
+    parameters {
+	    stringParam('APP_NAME', 'neo4j', '')
+    }
+
+    definition {
+	    cps {
+            sandbox()
+            script("""
+            node {
+                stage('Kubernetes: delete') {
+                    build job: 'madcore.kubectl.delete', parameters: [string(name: 'FILENAME', value: 'neo4j/kub')]
+                }
+                stage('Neo4j: wait for neo4j cluster to stop') {
+                    build job: 'madcore.kubectl.wait.service.down', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: 'neo4j-master:7474'), string(name: 'SERVICE_NAMESPACE', value: 'neo4j-cluster')]
+                }
+                stage('Neo4jCluster: nodes cleanup') {
+                    build job: 'madcore.kubectl.cluster.nodes.cleanup', parameters: [string(name: 'NODE_LABEL', value: 'cluster=k8s')]
+                }
+                }
+            """.stripIndent())
+	    }
+    }
+}
---
+++ neo4j/jobs/madcore_plugin_neo4j_deploy.groovy
@@ -0,0 +1,42 @@
+pipelineJob('madcore.plugin.neo4j.deploy') {
+    parameters {
+	    stringParam('APP_NAME', 'neo4j', '')
+      stringParam('S3BUCKETNAME', '', 'S3 bucket name for backup')
+    }
+
+    definition {
+	    cps {
+            sandbox()
+            script("""
+            node {
+                stage('Ingress: cloud formation install') {
+                }
+                stage('Neo4j: create services') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/services')]
+                }
+                stage('Neo4j: create master') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-master-rc.yaml')]
+                }
+                stage('Neo4j: wait for master') {
+                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'neo4j-master')]
+                }
+                stage('Neo4j: create client') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-client-rc.yaml')]
+                }
+                stage('Neo4j: wait for client') {
+                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'neo4j-client')]
+                }
+                stage('Neo4j: create data') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-data-rc.yaml')]
+                }
+                stage('Neo4j: wait for neo4j cluster to start') {
+                    build job: 'madcore.kubectl.wait.service.up', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: 'neo4j-main:7474'), string(name: 'SERVICE_NAMESPACE', value: 'neo4j-cluster')]
+                }
+                stage('Update HAproxy') {
+                    build job: 'madcore.ssl.letsencrypt.getandinstall', parameters: [string(name: 'S3BucketName', value: params.S3BUCKETNAME)]
+                }
+              }
+            """.stripIndent())
+	    }
+    }
+}

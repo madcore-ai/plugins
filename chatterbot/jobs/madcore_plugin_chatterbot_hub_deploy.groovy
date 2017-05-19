1b4e5a66b6c0146ba5783cae5bad5dfe6329af41
nijap <nijap@techversantinfotech.com>
2017-05-19 15:25:15 +0530

    chatterbot plugin files

---
+++ chatterbot/jobs/madcore_plugin_chatterbot_hub_deploy.groovy
@@ -0,0 +1,54 @@
+pipelineJob('madcore.plugin.chatterbot.deploy') {
+    parameters {
+      stringParam('REPO_URL', 'https://github.com/madcore-ai/containers', '')
+      stringParam('REPO_BRANCH', 'issue-124-chatterbot', '')
+      stringParam('APP_NAME', 'chatterbot', '')
+      stringParam('SERVICE_PORT', '9019', '')
+      stringParam('DOCKERFILE_PATH', 'chatterbot', 'Specify path to docker file relative to root repo.')
+      stringParam('S3BUCKETNAME', '', 'S3 bucket name for backup')
+      stringParam('APP_NAMESPACE', 'chatterbot-plugin', 'Plugin namespase')
+      stringParam('APP_SERVICE_NAME', 'chatterbot-service', 'Plugin service name')
+      booleanParam('MADCORE_INGRESS_FLAG', false, 'ingress flag ')
+    }
+
+    definition {
+      cps {
+            sandbox()
+            script("""
+            node {
+                stage ('Docker: build container') {
+                  build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'DOCKERFILE_PATH', value: params.DOCKERFILE_PATH)]
+                }
+                stage ('Docker: registry publish') {
+                  build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                }
+                stage ('Docker: registry status') {
+                  build job: 'madcore.docker.registry.status', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                }
+                stage ('Generate rc yaml') {
+                  build job: 'madcore.plugin.chatterbot.render.template', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
+                }
+                stage ('Kubernetes: create') {
+                  build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'chatterbot/kub')]
+                }
+                stage ('Update app base') {
+                  APP_SERVICE_NAME = params.APP_NAME + "-service"
+                  build job: 'madcore.redis.app.update', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_PORT', value: params.SERVICE_PORT), string(name: 'APP_NAMESPACE', value: params.APP_NAMESPACE), string(name: 'APP_SERVICE_NAME', value: APP_SERVICE_NAME) ]
+                }
+                stage ('Update CSR') {
+                  build job: 'madcore.ssl.csr.generate'
+                }
+                stage ('Update certificate and haproxy') {
+                  build job: 'madcore.ssl.letsencrypt.getandinstall', parameters: [string(name: 'S3BucketName', value: params.S3BUCKETNAME)]
+                }
+                stage ('add to ingress controller') {
+                  if (params.MADCORE_INGRESS_FLAG == true) {
+                    build job: 'madcore.plugin.ingress.add.service', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: APP_SERVICE_NAME), string(name: 'SERVICE_PORT', value: params.SERVICE_PORT), string(name: 'SERVICE_NAMESPACE', value: params.APP_NAMESPACE) ]
+                  }
+                  else {println "not need add to ingress controller"}
+                }
+              }
+            """.stripIndent())
+      }
+    }
+}

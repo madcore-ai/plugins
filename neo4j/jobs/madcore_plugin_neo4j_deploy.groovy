0bb35eccc1f3c69e9d52ce64f08f7a2cb01fe565
Nand Singh <nand@madcore.ai>
2017-05-19 05:53:29 +0100

    Initial changes for neo4j

--- neo4j/jobs/madcore_plugin_neo4j_deploy.groovy
+++ neo4j/jobs/madcore_plugin_neo4j_deploy.groovy
@@ -11,23 +11,17 @@ pipelineJob('madcore.plugin.neo4j.deploy') {
             node {
                 stage('Ingress: cloud formation install') {
                 }
+                stage('Neo4j: create volume') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j.yaml')]
+                }
                 stage('Neo4j: create services') {
                     build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/services')]
                 }
-                stage('Neo4j: create master') {
-                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-master-rc.yaml')]
+                stage('Neo4j: create core') {
+                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-core.yaml')]
                 }
-                stage('Neo4j: wait for master') {
-                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'neo4j-master')]
-                }
-                stage('Neo4j: create client') {
-                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-client-rc.yaml')]
-                }
-                stage('Neo4j: wait for client') {
-                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'neo4j-client')]
-                }
-                stage('Neo4j: create data') {
-                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j-data-rc.yaml')]
+                stage('Neo4j: wait for core') {
+                    build job: 'madcore.kubectl.wait.pod.up', parameters: [string(name: 'POD_NAME', value: 'neo4j-core')]
                 }
                 stage('Neo4j: wait for neo4j cluster to start') {
                     build job: 'madcore.kubectl.wait.service.up', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: 'neo4j-main:7474'), string(name: 'SERVICE_NAMESPACE', value: 'neo4j-cluster')]

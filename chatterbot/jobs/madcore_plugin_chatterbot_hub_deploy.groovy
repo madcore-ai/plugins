e290560d7d116e216e185c89751a32dd9f043032
nijap <nijap@techversantinfotech.com>
2017-05-23 11:07:18 +0530

    chatterbot plugin changes

--- chatterbot/jobs/madcore_plugin_chatterbot_hub_deploy.groovy
+++ chatterbot/jobs/madcore_plugin_chatterbot_hub_deploy.groovy
@@ -1,14 +1,12 @@
 pipelineJob('madcore.plugin.chatterbot.deploy') {
     parameters {
-      stringParam('REPO_URL', 'https://github.com/madcore-ai/containers', '')
-      stringParam('REPO_BRANCH', 'issue-124-chatterbot', '')
       stringParam('APP_NAME', 'chatterbot', '')
       stringParam('SERVICE_PORT', '9019', '')
-      stringParam('DOCKERFILE_PATH', 'chatterbot', 'Specify path to docker file relative to root repo.')
       stringParam('S3BUCKETNAME', '', 'S3 bucket name for backup')
-      stringParam('APP_NAMESPACE', 'chatterbot-plugin', 'Plugin namespase')
-      stringParam('APP_SERVICE_NAME', 'chatterbot-service', 'Plugin service name')
       booleanParam('MADCORE_INGRESS_FLAG', false, 'ingress flag ')
+      stringParam('APP_NAMESPACE', 'chatterbot', 'Plugin namespase')
+      stringParam('APP_SERVICE_NAME', 'chatterbot', 'Plugin service name')
+
     }
 
     definition {
@@ -16,24 +14,11 @@ pipelineJob('madcore.plugin.chatterbot.deploy') {
             sandbox()
             script("""
             node {
-                stage ('Docker: build container') {
-                  build job: 'madcore.docker.image.build', parameters: [string(name: 'REPO_URL', value: params.REPO_URL), string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'DOCKERFILE_PATH', value: params.DOCKERFILE_PATH)]
-                }
-                stage ('Docker: registry publish') {
-                  build job: 'madcore.docker.registry.publish', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
-                }
-                stage ('Docker: registry status') {
-                  build job: 'madcore.docker.registry.status', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
-                }
-                stage ('Generate rc yaml') {
-                  build job: 'madcore.plugin.chatterbot.render.template', parameters: [string(name: 'APP_NAME', value: params.APP_NAME)]
-                }
                 stage ('Kubernetes: create') {
                   build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'chatterbot/kub')]
                 }
                 stage ('Update app base') {
-                  APP_SERVICE_NAME = params.APP_NAME + "-service"
-                  build job: 'madcore.redis.app.update', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_PORT', value: params.SERVICE_PORT), string(name: 'APP_NAMESPACE', value: params.APP_NAMESPACE), string(name: 'APP_SERVICE_NAME', value: APP_SERVICE_NAME) ]
+                  build job: 'madcore.redis.app.update', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_PORT', value: params.SERVICE_PORT), string(name: 'APP_NAMESPACE', value: params.APP_NAMESPACE), string(name: 'APP_SERVICE_NAME', value: params.APP_SERVICE_NAME) ]
                 }
                 stage ('Update CSR') {
                   build job: 'madcore.ssl.csr.generate'
@@ -43,7 +28,7 @@ pipelineJob('madcore.plugin.chatterbot.deploy') {
                 }
                 stage ('add to ingress controller') {
                   if (params.MADCORE_INGRESS_FLAG == true) {
-                    build job: 'madcore.plugin.ingress.add.service', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: APP_SERVICE_NAME), string(name: 'SERVICE_PORT', value: params.SERVICE_PORT), string(name: 'SERVICE_NAMESPACE', value: params.APP_NAMESPACE) ]
+                    build job: 'madcore.plugin.ingress.add.service', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: params.APP_SERVICE_NAME), string(name: 'SERVICE_PORT', value: '9019'), string(name: 'SERVICE_NAMESPACE', value: params.APP_NAMESPACE ) ]
                   }
                   else {println "not need add to ingress controller"}
                 }

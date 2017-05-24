47f6799dafc473380ad81c837041abb7d569f91a
Denys Sokolov <hantden@gmail.com>
2017-05-24 12:31:04 +0300

    deleted unnessecary files and steps

--- neo4j/jobs/madcore_plugin_neo4j_deploy.groovy
+++ neo4j/jobs/madcore_plugin_neo4j_deploy.groovy
@@ -1,7 +1,6 @@
 pipelineJob('madcore.plugin.neo4j.deploy') {
     parameters {
 	    stringParam('APP_NAME', 'neo4j', '')
-      stringParam('S3BUCKETNAME', '', 'S3 bucket name for backup')
     }
 
     definition {
@@ -9,10 +8,7 @@ pipelineJob('madcore.plugin.neo4j.deploy') {
             sandbox()
             script("""
             node {
-                stage('Ingress: cloud formation install') {
-                }
-                stage('Neo4j: create volume') {
-                    build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/neo4j.yaml')]
+                stage('Neo4j: cloud formation install') {
                 }
                 stage('Neo4j: create services') {
                     build job: 'madcore.kubectl.create', parameters: [string(name: 'FILENAME', value: 'neo4j/kub/services')]
@@ -22,9 +18,6 @@ pipelineJob('madcore.plugin.neo4j.deploy') {
                 }
                 stage('Neo4j: wait for neo4j cluster to start') {
                     build job: 'madcore.kubectl.wait.service.up', parameters: [string(name: 'APP_NAME', value: params.APP_NAME), string(name: 'SERVICE_NAME', value: 'neo4j:7474'), string(name: 'SERVICE_NAMESPACE', value: 'neo4j-cluster')]
-                }
-                stage('Update HAproxy') {
-                    build job: 'madcore.ssl.letsencrypt.getandinstall', parameters: [string(name: 'S3BucketName', value: params.S3BUCKETNAME)]
                 }
               }
             """.stripIndent())

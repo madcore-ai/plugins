04e01f3dd5dbb405da3e8ec6e437a3c0f0bc33bd
Denys Sokolov <hantden@gmail.com>
2017-05-26 21:11:14 +0300

    add check job

--- kafka/jobs/madcore_plugin_kafka_status.groovy
+++ kafka/jobs/madcore_plugin_kafka_status.groovy
@@ -8,8 +8,11 @@ pipelineJob('madcore.plugin.kafka.status') {
             sandbox()
             script("""
             node {
-                stage ('docker registry status') {
+                stage ('helm chart status') {
                   build job: 'madcore.helm.status', parameters: [string(name: 'RELEASE_NAME', value: params.RELEASE_NAME)]
+                }
+                stage ('zookeeper clients status') {
+                  build job: 'madcore.plugin.kafka.test', parameters: [string(name: 'RELEASE_NAME', value: params.RELEASE_NAME)]
                 }
               }
             """.stripIndent())

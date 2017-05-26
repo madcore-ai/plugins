1135311ee78e682a364357282d693515150de4df
Denys Sokolov <hantden@gmail.com>
2017-05-26 13:40:10 +0300

    fix syntax in neo4j selftest job

--- neo4j/jobs/madcore_plugin_neo4j_selftest.groovy
+++ neo4j/jobs/madcore_plugin_neo4j_selftest.groovy
@@ -4,7 +4,7 @@ job('madcore.plugin.neo4j.selftest') {
         def command = """#!/bin/bash
 
 kubectl exec neo4j-core-0 --namespace=neo4j-cluster -- bin/cypher-shell "CALL dbms.cluster.overview()"
-
+"""
         shell(command)
     }
 }

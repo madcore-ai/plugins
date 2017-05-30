2c84932c0ae9b67a2f93954de6545eaebc667f58
Taras <php.laboratory@gmail.com>
2017-05-30 07:35:56 +0300

    Set secrets account

--- offlineimap/jobs/madcore_plugin_offlineimap_deploy.groovy
+++ offlineimap/jobs/madcore_plugin_offlineimap_deploy.groovy
@@ -5,11 +5,11 @@ pipelineJob('madcore.plugin.offlineimap.deploy') {
       stringParam('APP_NAME', 'offlineimap', '')
       stringParam('DOCKERFILE_PATH', 'offlineimap', 'Specify path to docker file relative to root repo.')
       stringParam('APP_NAMESPACE', 'offlineimap-plugin', 'Plugin namespase')
-
-      stringParam('REMOTE_USER', 'coxodox@gmail.com', '')
-      stringParam('OAUTH2_CLIENT_ID', '846427981560-l7a2oeb1m29moj183l9g0ruq0vlbbuic.apps.googleusercontent.com', '')
-      stringParam('OAUTH2_CLIENT_SECRET', 'YjYdktcPwa-RBomNZvkuuTMx', '')
-      stringParam('OAUTH2_REFRESH_TOKEN', '1/yR5OP7m9DnSY6oN7wvVhOYXQNSp9k9cYkjupdcyWLsjkWA7wEA-rykT_11sJkyyX', '')
+      stringParam('ACCOUNT', 'mysecret', '')
+//      stringParam('REMOTE_USER', 'coxodox@gmail.com', '')
+//      stringParam('OAUTH2_CLIENT_ID', '846427981560-l7a2oeb1m29moj183l9g0ruq0vlbbuic.apps.googleusercontent.com', '')
+//      stringParam('OAUTH2_CLIENT_SECRET', 'YjYdktcPwa-RBomNZvkuuTMx', '')
+//      stringParam('OAUTH2_REFRESH_TOKEN', '1/yR5OP7m9DnSY6oN7wvVhOYXQNSp9k9cYkjupdcyWLsjkWA7wEA-rykT_11sJkyyX', '')
 
 
 
@@ -33,12 +33,7 @@ pipelineJob('madcore.plugin.offlineimap.deploy') {
                 stage ('Generate rc yaml') {
                   build job: 'madcore.plugin.offlineimap.render.template', parameters: [
                     string(name: 'APP_NAME', value: params.APP_NAME),
-                    string(name: 'REMOTE_USER', value: params.REMOTE_USER),
-                    string(name: 'OAUTH2_CLIENT_ID', value: params.OAUTH2_CLIENT_ID),
-                    string(name: 'OAUTH2_CLIENT_SECRET', value: params.OAUTH2_CLIENT_SECRET),
-                    string(name: 'OAUTH2_REFRESH_TOKEN', value: params.OAUTH2_REFRESH_TOKEN)
-
-
+                    string(name: 'ACCOUNT', value: params.ACCOUNT),
                     ]
                 }
                 stage ('Kubernetes: create') {
--- offlineimap/jobs/madcore_plugin_offlineimap_render_template.groovy
+++ offlineimap/jobs/madcore_plugin_offlineimap_render_template.groovy
@@ -1,10 +1,7 @@
 pipelineJob('madcore.plugin.offlineimap.render.template') {
     parameters {
       stringParam('APP_NAME', 'offlineimap', '')
-        stringParam('REMOTE_USER', 'coxodox@gmail.com', '')
-        stringParam('OAUTH2_CLIENT_ID', '846427981560-l7a2oeb1m29moj183l9g0ruq0vlbbuic.apps.googleusercontent.com', '')
-        stringParam('OAUTH2_CLIENT_SECRET', 'YjYdktcPwa-RBomNZvkuuTMx', '')
-        stringParam('OAUTH2_REFRESH_TOKEN', '1/yR5OP7m9DnSY6oN7wvVhOYXQNSp9k9cYkjupdcyWLsjkWA7wEA-rykT_11sJkyyX', '')
+      stringParam('ACCOUNT', 'mysecret', '')
 
 
     }
@@ -16,7 +13,7 @@ pipelineJob('madcore.plugin.offlineimap.render.template') {
               script("""
               node {
                 stage ('rendering rc template') {
-                  sh 'cat /opt/plugins/offlineimap/templates/rc.yaml.template |sed -e "s/{APP_NAME_TMPL}/\$APP_NAME/g" > /opt/plugins/offlineimap/kub/rc.yaml'
+                  sh 'cat /opt/plugins/offlineimap/templates/rc.yaml.template |sed -e "s/{APP_NAME_TMPL}/\$APP_NAME/g |sed -e "s/{SECRET_TMPL}/\$ACCOUNT/g" > /opt/plugins/offlineimap/kub/rc.yaml'
                 }
               }
               """.stripIndent())

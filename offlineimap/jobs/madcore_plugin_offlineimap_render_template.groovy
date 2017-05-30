2143a7a3375f3f0740e9d3fe5ed6fba1de1c1181
Taras <php.laboratory@gmail.com>
2017-05-30 09:06:53 +0300

    small type error

--- offlineimap/jobs/madcore_plugin_offlineimap_render_template.groovy
+++ offlineimap/jobs/madcore_plugin_offlineimap_render_template.groovy
@@ -13,7 +13,7 @@ pipelineJob('madcore.plugin.offlineimap.render.template') {
               script("""
               node {
                 stage ('rendering rc template') {
-                  sh 'cat /opt/plugins/offlineimap/templates/rc.yaml.template |sed -e "s/{APP_NAME_TMPL}/\$APP_NAME/g |sed -e "s/{SECRET_TMPL}/\$ACCOUNT/g" > /opt/plugins/offlineimap/kub/rc.yaml'
+                  sh 'cat /opt/plugins/offlineimap/templates/rc.yaml.template |sed -e "s/{APP_NAME_TMPL}/\$APP_NAME/g" |sed -e "s/{SECRET_TMPL}/\$ACCOUNT/g" > /opt/plugins/offlineimap/kub/rc.yaml'
                 }
               }
               """.stripIndent())
